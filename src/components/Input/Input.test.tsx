2021875c82516e76e86cc79f0f6466f21a06aaee
Claudia Hobus <clauu013@gmail.com>
2025-03-06 17:35:17 -0300

    Cria testes para componente Input

---
+++ src/components/Input/Input.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react-native";
+
+import { Input } from "@components/Input";
+
+describe("Component: Input", () => {
+    it("should be render without activity indicator if isLoading prop is undefined", () => {
+        // const { debug } = render(<Input/>);
+        // debug();
+        render(<Input/>);
+
+        const activityIndicator = screen.queryByTestId('activity-indicator');
+        expect(activityIndicator).toBeNull();
+    });
+
+    it("should be render with activity indicator if isLoading prop is true", () => {
+        render(<Input isLoading/>);
+
+        const activityIndicator = screen.getByTestId('activity-indicator');
+        expect(activityIndicator).toBeTruthy();
+    })
+});
